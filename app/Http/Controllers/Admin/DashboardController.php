da99c5a2abacef7fbdd56c5e54cc41acea32d6f6
Fuzail21 <fuzailsiddiqui57@gmail.com>
2026-01-31 22:43:09 +0500

    Version 1.0

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -15,8 +15,7 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index()
-    {
+    public function index(){
         $user = auth()->user();
 
         // For managers, scope data to their assigned cities
